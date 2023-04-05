d3822833511954154ce73d8814d3e0051bf55d3c
Philippe Karan <philippe.karan@usherbrooke.ca>
2023-04-05 17:44:15 -0400

    WIP

---
+++ run_all_save_masks.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+cd /home/pkaran/Samsung/data/MT_Diffusion/myelo_inferno/DTI_metrics;
+all_dirs=$(ls);
+
+cd ../output;
+
+for dir in $all_dirs;
+    do python ~/source/mt_diffusion/save_angle_bins_mask.py ../DTI_metrics/${dir}/${dir}__dti_evecs_v1.nii.gz ../wm_mask/${dir}/${dir}__wm_mask.nii.gz;
+    echo $dir;
+done;
