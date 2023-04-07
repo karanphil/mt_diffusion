563597605ca3646af47a9dec51bd5bd4cca98227
Philippe Karan <philippe.karan@usherbrooke.ca>
2023-04-07 13:02:35 -0400

    WIP

--- average_mt_ihmt_by_angle_fodf.py
+++ average_mt_ihmt_by_angle_fodf.py
@@ -63,9 +63,6 @@ sub_ses_dir = Path(peaks_nifti.parent.name)
 if not sub_ses_dir.is_dir():
     sub_ses_dir.mkdir()
 
-# print(peaks_data[45,86,84])
-print(peaks_data[45,82,87])
-
 nb_peaks = int(peaks_data.shape[-1] / 3)
 peaks_norm = np.zeros((peaks_data.shape[:-1] + (nb_peaks,)))
 for i in range(nb_peaks):
@@ -73,14 +70,9 @@ for i in range(nb_peaks):
     for j in range(3):
         peaks_data[..., i * 3 + j] /= peaks_norm[..., i]
 
-# print(peaks_data[45,86,84])
-# print(peaks_norm[45,86,84])
-# print(peak_values_data[45,86,84])
-
-print(peaks_norm[45,82,87])
-
 # bins_width = [1, 3]
-bins_width = [1]
+bins_width = [10]
+norm_thr = 0.7
 
 for w in bins_width: # width of the angle bins
 
@@ -101,44 +93,16 @@ for w in bins_width: # width of the angle bins
 
     # Apply the WM mask
     wm_mask_bool = (wm_mask_data > 0.9) & (nufo_data == 2)
-    norm_mask_bool = (peaks_norm[..., 0] > 0.9) & (peaks_norm[..., 1] > 0.9)
+    norm_mask_bool = (peaks_norm[..., 0] > norm_thr) & (peaks_norm[..., 1] > norm_thr)
 
-    toto_mask = wm_mask_bool & norm_mask_bool
-
-    # print(wm_mask_data[45,86,84] > 0.9)
-    # print(nufo_data[52,98,86] == 2)
-    # print(wm_mask_bool[52,98,86])
-    # print(norm_mask_bool[52,98,86])
-    # print(peaks_norm[52,98,86, 0] > 0.9)
-    # print(peaks_norm[52,98,86, 1] > 0.9)
-
-    # nib.save(nib.Nifti1Image(toto_mask.astype(np.uint8), peaks_img.affine), str(sub_ses_dir / "toto_mask.nii.gz"))
-    # nib.save(nib.Nifti1Image(wm_mask_bool.astype(np.uint8), peaks_img.affine), str(sub_ses_dir / "wm_mask_nufo2.nii.gz"))
-    # nib.save(nib.Nifti1Image(norm_mask_bool.astype(np.uint8), peaks_img.affine), str(sub_ses_dir / "norm_mask.nii.gz"))
-    # nib.save(nib.Nifti1Image(peaks_norm[..., 0], peaks_img.affine), str(sub_ses_dir / "peaks_norm_0.nii.gz"))
-    # nib.save(nib.Nifti1Image(peaks_norm[..., 1], peaks_img.affine), str(sub_ses_dir / "peaks_norm_1.nii.gz"))
-
-    print("Step 1:")
-    print(peaks_data[45,82,87])
-    print(theta_f1[45,82,87])
-    print(theta_f2[45,82,87])
-
-    mask_diag = (np.zeros(nufo_data.shape) > 0)
     for i in range(len(bins) - 1):
         angle_mask_0_90 = (theta_f1 >= bins[i]) & (theta_f1 < bins[i+1])
         angle_mask_90_180 = (180 - theta_f1 >= bins[i]) & (180 - theta_f1 < bins[i+1])
-        print("bins i : ", bins[i], bins[i+1])
-        print("f1 0_90 :", angle_mask_0_90[45,82,87])
-        print("f1 90_180 :", angle_mask_90_180[45,82,87])
         angle_mask = angle_mask_0_90 | angle_mask_90_180
         mask_f1 = angle_mask
         for j in range(len(bins) - 1):
             angle_mask_0_90 = (theta_f2 >= bins[j]) & (theta_f2 < bins[j+1]) 
             angle_mask_90_180 = (180 - theta_f2 >= bins[j]) & (180 - theta_f2 < bins[j+1])
-            if i==j:
-                print("bins j : ", bins[j], bins[j+1])
-                print("f2 0_90 :", angle_mask_0_90[45,82,87])
-                print("f2 90_180 :", angle_mask_90_180[45,82,87])
             angle_mask = angle_mask_0_90 | angle_mask_90_180
             mask_f2 = angle_mask
             mask = mask_f1 & mask_f2 & wm_mask_bool & norm_mask_bool
@@ -149,13 +113,6 @@ for w in bins_width: # width of the angle bins
             else:
                 mtr_means[i, j] = np.mean(mtr_data[mask])
                 ihmtr_means[i, j] = np.mean(ihmtr_data[mask])
-            if i == j:
-                print(np.sum(mask))
-                mask_diag = mask_diag | mask
-
-    # nib.save(nib.Nifti1Image(mask_diag.astype(np.uint8), peaks_img.affine), str(sub_ses_dir / "mask_diag.nii.gz"))
-
-    print("Step 2:")
     
     for i in range(len(bins) - 1):
         for j in range(i):
@@ -166,52 +123,53 @@ for w in bins_width: # width of the angle bins
             nb_voxels[i, j] = nb_voxels[i, j] + nb_voxels[j, i]
             nb_voxels[j, i] = nb_voxels[i, j]
 
-    # print(nb_voxels)
-    print(np.diagonal(nb_voxels))
+    mtr_means_diag = np.diagonal(mtr_means)
+    ihmtr_means_diag = np.diagonal(ihmtr_means)
+    nb_voxels_diag = np.diagonal(nb_voxels)
 
     # Save the results to a text file
     results = np.column_stack((bins[:-1], bins[1:], mtr_means, ihmtr_means, nb_voxels))
     txt_name = "results_" + str(w) + "_degrees_bins.txt"
     txt_path = sub_ses_dir / txt_name
+    # !!!!!!!! À sauver en disant 3D ou 2 fibers ou ailleurs!!!!!!!
     # if ratios:
-        # np.savetxt(str(txt_path), results, fmt='%10.5f', delimiter='\t', header='Angle_min\tAngle_max\tMTR_mean\tihMTR_mean\tNb_voxels')
+    #     np.savetxt(str(txt_path), results, fmt='%10.5f', delimiter='\t', header='Angle_min\tAngle_max\tMTR_mean\tihMTR_mean\tNb_voxels')
     # elif sats:
-        # np.savetxt(str(txt_path), results, fmt='%10.5f', delimiter='\t', header='Angle_min\tAngle_max\tMTsat_mean\tihMTsat_mean\tNb_voxels')
+    #     np.savetxt(str(txt_path), results, fmt='%10.5f', delimiter='\t', header='Angle_min\tAngle_max\tMTsat_mean\tihMTsat_mean\tNb_voxels')
+
+    max_count = np.max(nb_voxels_diag)
+    norm = mpl.colors.Normalize(vmin=0, vmax=max_count)
+    plot_init()
+    fig, (ax1, ax2, cax) = plt.subplots(1, 3, gridspec_kw={"width_ratios":[1, 1, 0.05]})
+    ax1.scatter(bins[:-1], mtr_means_diag, c=nb_voxels_diag, cmap='Greys', norm=norm, edgecolors='black', linewidths=1)
+    colorbar = ax1.set_xlabel('Angle between e1 and B0 field (degrees)')
+    if ratios:
+        ax1.set_ylabel('MTR mean')
+        ax1.set_title('MTR vs Angle')
+    elif sats:
+        ax1.set_ylabel('MTsat mean')
+        ax1.set_title('MTsat vs Angle')
+    colorbar = ax2.scatter(bins[:-1], ihmtr_means_diag, c=nb_voxels_diag, cmap='Greys', norm=norm, edgecolors='black', linewidths=1)
+    ax2.set_xlabel('Angle between e1 and B0 field (degrees)')
+    if ratios:
+        ax2.set_ylabel('ihMTR mean')
+        ax2.set_title('ihMTR vs Angle')
+    elif sats:
+        ax2.set_ylabel('ihMTsat mean')
+        ax2.set_title('ihMTsat vs Angle')
+    fig.colorbar(colorbar, cax=cax, label="Voxel count")
+    fig.tight_layout()
+    plt.show()
+    fig_name = "plot_" + str(j) + "_degrees_bins.png"
+    fig_path = sub_ses_dir / fig_name
+    # plt.savefig(fig_path, dpi=300)
+    plt.close()
 
     # Plot the results
-    # print("Step 3:")
-
-    # hf = plt.figure()
-    # ha = hf.add_subplot(111, projection='3d')
-
-    # X, Y = np.meshgrid(bins[:-1], bins[:-1])  # `plot_surface` expects `x` and `y` data to be 2D
-    # ha.plot_surface(X, Y, nb_voxels)
-
-    # plt.show()
-    # max_count = np.max(nb_voxels)
-    # norm = mpl.colors.Normalize(vmin=0, vmax=max_count)
-    # plot_init()
-    # fig, (ax1, cax) = plt.subplots(1, 2, gridspec_kw={"width_ratios":[1, 0.05]})
-    # ax1.scatter(bins[:-1], bins[:-1], mtr_means, c=nb_voxels, cmap='Greys', norm=norm, edgecolors='black', linewidths=1)
-    # colorbar = ax1.set_xlabel('Angle between e1 and B0 field (degrees)')
-    # if ratios:
-    #     ax1.set_ylabel('MTR mean')
-    #     ax1.set_title('MTR vs Angle')
-    # elif sats:
-    #     ax1.set_ylabel('MTsat mean')
-    #     ax1.set_title('MTsat vs Angle')
-    # # colorbar = ax2.scatter(bins[:-1], bins[:-1], ihmtr_means, c=nb_voxels, cmap='Greys', norm=norm, edgecolors='black', linewidths=1)
-    # # ax2.set_xlabel('Angle between e1 and B0 field (degrees)')
-    # # if ratios:
-    # #     ax2.set_ylabel('ihMTR mean')
-    # #     ax2.set_title('ihMTR vs Angle')
-    # # elif sats:
-    # #     ax2.set_ylabel('ihMTsat mean')
-    # #     ax2.set_title('ihMTsat vs Angle')
-    # fig.colorbar(colorbar, cax=cax, label="Voxel count")
-    # fig.tight_layout()
-    # plt.show()
-    # fig_name = "plot_" + str(j) + "_degrees_bins.png"
-    # fig_path = sub_ses_dir / fig_name
-    # # plt.savefig(fig_path, dpi=300)
-    # plt.close()
+    print("Step 3:")
+    hf = plt.figure()
+    ha = hf.add_subplot(111, projection='3d')
+    X, Y = np.meshgrid(bins[:-1], bins[:-1])
+    ha.plot_surface(X, Y, mtr_means)
+    plt.show()
+    plt.close()
