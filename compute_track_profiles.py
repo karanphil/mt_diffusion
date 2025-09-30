9a6776f893b8ebe38fa1ddf07fb1a8db98005399
Philippe Karan <philippe.karan@usherbrooke.ca>
2025-09-30 15:31:03 -0400

    WIP

--- compute_track_profiles.py
+++ compute_track_profiles.py
@@ -2,12 +2,14 @@
 # -*- coding: utf-8 -*-
 
 """
-
+Script to plot the track-profile of a bundle for both the MTR and the
+fixel-wise MTR.
 """
 
 import argparse
 import logging
 
+from matplotlib import pyplot as plt
 import nibabel as nib
 import numpy as np
 
@@ -22,11 +24,21 @@ def _build_arg_parser():
 
     p.add_argument('in_bundle_mtr')
 
+    p.add_argument('in_bundle_fixel_mtr')
+
     p.add_argument('in_bundle_labels')
 
-    p.add_argument('--in_bundle_mask')
-
     p.add_argument('out_dir')
+
+    p.add_argument('--in_bundle_map')
+
+    p.add_argument('--map_threshold', type=float, default=0.75,
+                   help='Threshold to apply to the bundle map to create a '
+                        'mask. Default is 0.1.')
+    
+    p.add_argument('--median', action='store_true',
+                   help='Use the median instead of the mean to compute the '
+                        'track-profile. Default is False.')
 
     add_verbose_arg(p)
     add_overwrite_arg(p)
@@ -41,24 +53,53 @@ def main():
     if args.verbose:
         logging.basicConfig(level=logging.INFO)
 
-    assert_inputs_exist(parser, [args.in_mtr, args.in_voxel_density_masks])
+    assert_inputs_exist(parser, [args.in_bundle_mtr, args.in_bundle_fixel_mtr,
+                                 args.in_bundle_labels])
     assert_outputs_exist(parser, args, [args.out_dir])
 
-    mtr_img = nib.load(args.in_mtr)
+    mtr_img = nib.load(args.in_bundle_mtr)
     mtr = mtr_img.get_fdata().astype(np.float32)
 
-    voxel_density_img = nib.load(args.in_voxel_density_masks)
-    bundles_mask = voxel_density_img.get_fdata().astype(bool)
+    fixel_mtr_img = nib.load(args.in_bundle_fixel_mtr)
+    fixel_mtr = fixel_mtr_img.get_fdata().astype(np.float32)
 
-    lut = np.loadtxt(args.in_LUT, dtype=str)
+    labels_img = nib.load(args.in_bundle_labels)
+    labels = labels_img.get_fdata().astype(np.uint8)
 
-    for i in range(bundles_mask.shape[-1]):
-        bundle_mtr = np.zeros(mtr.shape)
-        bundle_mtr[bundles_mask[..., i]] = mtr[bundles_mask[..., i]]
+    if args.in_bundle_map:
+        map_img = nib.load(args.in_bundle_map)
+        map = map_img.get_fdata().astype(np.float32)
+        mask = map >= args.map_threshold
+    else:
+        mask = np.ones(mtr.shape, dtype=bool)
 
-        bundle_name = lut[0][i]
+    unique_labels = np.unique(labels[mask])
+    unique_labels = unique_labels[unique_labels != 0]
 
-        nib.save(nib.Nifti1Image(bundle_mtr, mtr_img.affine), args.out_dir + f'/mtr_{bundle_name}.nii.gz')
+    fixel_mtr_profile = np.zeros((len(unique_labels),))
+    mtr_profile = np.zeros((len(unique_labels),))
+    for i, label in enumerate(unique_labels):
+        label_mask = (labels == label) & mask
+        if args.median:
+            fixel_mtr_profile[i] = np.median(fixel_mtr[label_mask])
+            mtr_profile[i] = np.median(mtr[label_mask])
+        else:
+            fixel_mtr_profile[i] = np.mean(fixel_mtr[label_mask])
+            mtr_profile[i] = np.mean(mtr[label_mask])
+        
+
+    plt.plot(unique_labels, mtr_profile, label='MTR', marker='o',
+             color='orange')
+    plt.plot(unique_labels, fixel_mtr_profile, label='Fixel-wise MTR',
+             marker='^', color='blue')
+    plt.xlabel('Bundle section')
+    if args.median:
+        plt.ylabel('Median MTR')
+    else:
+        plt.ylabel('Mean MTR')
+    plt.title('Track-profile of MTR and fixel-wise MTR')
+    plt.legend()
+    plt.show()
 
 
 if __name__ == "__main__":
