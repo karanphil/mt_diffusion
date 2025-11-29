e4ae3e44cb3ff3cb91f711ae01c14ad07c8e443d
Philippe Karan <philippe.karan@usherbrooke.ca>
2025-11-29 07:59:44 -0500

    WIP

--- python_scripts/plot_track_profiles_from_subjects.py
+++ python_scripts/plot_track_profiles_from_subjects.py
@@ -243,7 +243,7 @@ def main():
      ax3.set_yticks(np.arange(1, 6, 1))
      ax3.set_xlim(0, args.nb_sections + 1)
      ax3.set_xticks(np.arange(1, args.nb_sections + 1, 1))
-     ax3.legend(loc='upper right')
+     # ax3.legend(loc='upper right')
      # ax3.grid(alpha=0.3)
 
      plt.tight_layout()
